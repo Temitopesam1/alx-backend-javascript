41103f667cb291a236f9c9fb2041349c4ed09feb
Temitopesam1 <temitopesam5@gmail.com>
2023-03-21 01:20:30 +0000

    fix: Retouch code to work as expected

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,7 +1,3 @@
-function appendToEachArrayValue(array, appendString) {
-    let newArray = [];
-    for (let idx of array) {
-        newArray.push(`${appendString} ${idx}`);
-    }
-    return newArray;
+export default function concatArrays(array1, array2, string) {
+	return [...array1, array2, ...string];
 }
