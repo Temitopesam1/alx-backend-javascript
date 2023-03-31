81c432e3d9c40bbc683390d3f2fbf438409505cb
Temitopesam1 <temitopesam5@gmail.com>
2023-03-31 15:07:08 +0000

    doc: Add another way but in comments

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -13,4 +13,36 @@ export default class Building {
   set sqft(value) {
     this._sqft = value;
   }
+  
 }
+
+
+// Another way to do this that I figured after submission
+
+//export default class Building {
+//    constructor(sqft) {
+//        this._sqft = sqft;
+
+//    }
+
+//    get sqft() {
+//        return this._sqft;
+//    }
+
+//    set sqft(newSqft){
+//        if (typeof(newSqft) === 'number'){
+//            this._sqft = newSqft;
+//        }
+//        else {
+//            throw new Error('Name must a number')
+//        }
+//    }
+
+//    evacuationWarningMessage() {
+//        if (this.evacuationWarningMessage !== 'function') {
+//            throw Error('Class extending Building must override evacuationWarningMessage')
+//        }
+//    }
+//}
+
+
