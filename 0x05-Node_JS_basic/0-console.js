dc5303b9db32d3ef45d991248b5f926fbe1c8b68
Temitopesam1 <temitopesam5@gmail.com>
2023-05-17 14:15:15 +0000

    DOC: Add Readme file and Fix other minor issues

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(arg){
-	console.log(arg);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Abolarinwa Temitope sam <https://github.com/Temitopesam1>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
-readline = require('readline').createInterface({
-  	input: process.stdin,
-  	output: process.stdout
-});;
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-readline.question("Welcome to Holberton School, what is your name?\n", name => {
-	console.log(`Your name is: ${name}`);
-	readline.close()
-	process.stdin.on('end', () => {
-		console.log('This important software is now closing')
-	})
-})
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
